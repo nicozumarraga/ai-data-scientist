5605b3d875c9820dca9a093fd5398edd71f9f6a0
Nico Zumarraga <nicolaszumarraga9@gmail.com>
2024-11-18 22:23:45 +0100

    render file for deployment

--- backend/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Install TeX Live and required packages
-sudo apt-get update
-sudo apt-get install -y texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
