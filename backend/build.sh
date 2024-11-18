a15c0309e476a2587a29a6e57a9cdfc4238da272
Nico Zumarraga <nicolaszumarraga9@gmail.com>
2024-11-18 22:15:42 +0100

    Modified build.sh superuser

--- backend/build.sh
+++ backend/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Install TeX Live and required packages
-apt-get update
-apt-get install -y texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
+sudo apt-get update
+sudo apt-get install -y texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
